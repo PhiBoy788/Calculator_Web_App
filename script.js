0f0c804433f32f85d97e584635014c991b4aa4eb
PhiBoy788 <hkroll788@gmail.com>
2021-03-29 12:49:57 -0500

    Initial Commit

---
+++ script.js
@@ -0,0 +1,63 @@
+function add (num1, num2) {
+	return num1 + num2
+	
+}
+
+function subtract (num1, num2) {
+	return num1 - num2
+}
+
+function sum (numList) {
+	total = 0
+	for (i = 0; i < numList.length; i++){
+		total += numList[i]
+	}
+	return total
+}
+
+function multiply  (numList) {
+	total = 0
+	for (i = 1; i < numList.length; i++) {
+		if (i == 1){
+			total += numList[0] * numList[1]
+		}else{
+			total = total * numList[i]
+		}
+	}
+	return total
+}
+function power(num1,num2) {
+	total = 0
+	for (i = 1 ; i < num2; i++) {
+		if (total == 0) {
+			total = num1 * num1
+		}else{
+			total = total * num1
+		}
+	}
+return total
+
+}
+
+function factorial(num) {
+	num = Number(num)
+	total = 1
+	if (num == 0){
+		return 1
+	}else{
+		for(i = 1; i < num; i++){
+			total += total * i
+		}
+	}
+	return total
+	
+}
+
+module.exports = {
+	add,
+	subtract,
+	sum,
+	multiply,
+    power,
+	factorial
+}
